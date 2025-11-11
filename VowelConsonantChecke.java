61dfbeec8d863e17401b5261f7c1ff675d65a4b8
sanojkumar15480-dev <sanojkumar15480@gmail.com>
2025-11-11 12:27:18 +0530

    code

--- VowelConsonantChecke.java
+++ VowelConsonantChecke.java
@@ -1,25 +1,20 @@
 import java.util.Scanner;
 
-public class VowelConsonantChecke 
-{
-    public static void main(String args[])
-    {
+public class VowelConsonantChecke{
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a character:");
         char ch = scanner.next().charAt(0);
-        if(isVowel(ch))
-        {
+        if(isVowel(ch)){
             System.out.println(ch + " is a vowel.");
-        }
-        else
+        } else
         {
-            System.out.println(ch + "is a consonant.");
+            System.out.println(ch + " is a consonant.");
         }
         scanner.close();
     }
-        public static boolean isVowel(char ch)
-        {
-            ch = Character.toLowerCase(ch);
-            return ch  == 'a' || ch == 'e' || ch == 'i' || ch =='o' || ch == 'u';
-        }
+    public static boolean isVowel(char ch){
+        ch = Character.toLowerCase(ch);
+        return ch =='a' || ch == 'e' || ch == 'i' || ch =='o' || ch =='u';
+    }
 }
-
