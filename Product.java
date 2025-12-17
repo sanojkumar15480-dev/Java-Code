2969f345c4c54c23ba7984482f2fa95ff1522c29
sanojkumar15480-dev <sanojkumar15480@gmail.com>
2025-12-17 16:46:50 +0530

    sa

---
+++ Product.java
@@ -0,0 +1,32 @@
+class Product
+{
+    public int multiply(int a, int b)
+    {
+        int prod = a*b;
+        return prod;
+    }
+    public int multiply(int a, int b, int c)
+    {
+        int prod = a*b*c;
+        return prod;
+    }
+}
+class GFG
+{
+    public static void main(String[] args)
+    {
+        Product ob = new Product();
+        int prod1 = ob.multiply(1,2);
+        System.out.println("Product of the two integer value:"+ prod1);
+        int prod2 = ob.multiply(1,2,3);
+        // printing product of 3 numbers
+        System.out.println("Product of the three integer value:"+prod2);
+    }
+}
+
+
+
+
+
+    
+
