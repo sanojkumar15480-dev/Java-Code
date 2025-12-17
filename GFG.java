14197a764f3609c029c47e466f3975aac78fd83c
sanojkumar15480-dev <sanojkumar15480@gmail.com>
2025-12-17 18:41:43 +0530

    name

---
+++ GFG.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+class GFG{
+    public static void main(String args[])
+    {
+        Scanner myObj = new Scanner(System.in);
+    String userName;
+    System.out.println("Enter your name:");
+    userName = myObj.nextLine();
+    System.out.println("Your Name is: " + userName);
+    }
+}
